01d16ac58e3ec95e17fec6b952a26c5503aeac0a
Agnes <koinageagness@gmail.com>
2022-03-19 05:54:37 +0300

    add user interface section on js

--- js/scripts.js
+++ js/scripts.js
@@ -126,44 +126,111 @@ Pizza.prototype.calculateTotal = function () {
 };
 
 // user interface logic
+// $(document).ready(function() {
+//     $("form#new-order").submit(function(event) {
+//         event.preventDefault();
+
+//         var orderSize = $("input#new-size").val();
+//         var orderCrust= $("input#new-size").val();
+//         var orderToppings = $("input#new-size").val();
+//         var orderQuantity = $("input#new-size").val();
+
+
+//         var newOrder = new Order(inputtedFirstName, inputtedLastName);
+
+//         // append pizzas
+//     pizzas.forEach((pizza) => {
+//         $("#pizza").append(`<option value="${pizza.name}">${pizza.name}</option>`);
+//     });
+//     // append pizza sizes
+//     pizzaSizes.forEach((pizzaSize) => {
+//         $("#size").append(
+//             `<option value="${pizzaSize.size}">${pizzaSize.size}-${pizzaSize.price}</option>`
+//         );
+//     });
+
+//     // append pizza crusts
+//     pizzaCrusts.forEach((pizzaCrust) => {
+//         $("#crust").append(
+//             `<option value="${pizzaCrust.name}">${pizzaCrust.name}-${pizzaCrust.price}</option>`
+//         );
+//     });
+
+//     //append pizza toppings
+//     pizzaToppings.forEach((topping) => {
+//         $(".toppings").append(`<div class="col-md-6">
+//         <div class="form-check">
+//           <input class="form-check-input" name="toppings[]" type="checkbox" id="${topping}" value="${topping}">
+//           <label class="form-check-label" for="${topping}">
+//               ${topping}
+//           </label>
+//           </div>
+//         </div>`);
+// }
+
+
 $(document).ready(function() {
+
+    $("#add-order").click(function() {
+      $("#new-addresses").append('<form id="order-form">' +
+                                 '<div class="form-group">'+
+                                 '<label for="new-size">Select Pizza Size <span class="text-danger">*</span></label>' +
+                                 '<select name="size" id="size" class="form-control">'+
+                                     '<option value="" selected disabled>Size</option> ' +
+                                 '</select>' +
+                                 '</div>'+
+                                 '<div class="form-group">'+
+                                 '<label for="new-crust">Select Pizza Crust <span class="text-danger">*</span></label>'+
+                                 '<select name="crust" id="crust" class="form-control">'+
+                                 '<option value="" selected disabled>Select Crust</option>'+
+                                 '</select>'+
+                                 '</div>'+
+                                 '<div class="form-group">'+
+                                 '<label for="new-toppings">Select Pizza Toppings <span class="text-danger">*</span></label>'+
+                                 '<select name="toppings" id="toppings" class="form-control">'+
+                                 '<option value="" selected disabled>Toppings</option>'+
+                                     '</select>'+
+                                     '</div>');
+    });
+  
     $("form#new-order").submit(function(event) {
-        event.preventDefault();
-
-        var orderSize = $("input#new-size").val();
-        var orderCrust= $("input#new-size").val();
-        var orderToppings = $("input#new-size").val();
-        var orderQuantity = $("input#new-size").val();
-
-
-        var newOrder = new Order(inputtedFirstName, inputtedLastName);
-
-        // append pizzas
-    pizzas.forEach((pizza) => {
-        $("#pizza").append(`<option value="${pizza.name}">${pizza.name}</option>`);
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $(".new-address").each(function() {
+        var inputtedStreet = $(this).find("input.new-street").val();
+        var inputtedCity = $(this).find("input.new-city").val();
+        var inputtedCounty = $(this).find("input.new-county").val();
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
+        newContact.addresses.push(newAddress)
+      });
+  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+  
+      Address.prototype.fullAddress = function() {
+        return this.street + ", " + this.city + ", " + this.county;
+      }
+  
+      $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        });
+      });
+  
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-county").val("");
+  
     });
-    // append pizza sizes
-    pizzaSizes.forEach((pizzaSize) => {
-        $("#size").append(
-            `<option value="${pizzaSize.size}">${pizzaSize.size}-${pizzaSize.price}</option>`
-        );
-    });
-
-    // append pizza crusts
-    pizzaCrusts.forEach((pizzaCrust) => {
-        $("#crust").append(
-            `<option value="${pizzaCrust.name}">${pizzaCrust.name}-${pizzaCrust.price}</option>`
-        );
-    });
-
-    //append pizza toppings
-    pizzaToppings.forEach((topping) => {
-        $(".toppings").append(`<div class="col-md-6">
-        <div class="form-check">
-          <input class="form-check-input" name="toppings[]" type="checkbox" id="${topping}" value="${topping}">
-          <label class="form-check-label" for="${topping}">
-              ${topping}
-          </label>
-          </div>
-        </div>`);
-}
+  });
+  
